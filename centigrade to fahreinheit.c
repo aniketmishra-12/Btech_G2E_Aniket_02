acb5e5d1579d838485090ea1b16acc367f546b92
aniketmishra-12 <aniketroyal0000@gmail.com>
2026-03-24 15:58:06 +0530

    Create centigrade to fahreinheit.c

---
+++ centigrade to fahreinheit.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float c,f;
+    printf("Enter the temperaturein c");
+    scanf("%f",&c);
+    f=(c/5*9+32);
+    printf("The converted temperature in f is %f",f);
+    return 0;
+    
+}
