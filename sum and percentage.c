18ef58368fbee55a37999556128fec0f6931acbc
aniketmishra-12 <aniketroyal0000@gmail.com>
2026-03-24 14:47:25 +0530

    Create sum and percentage.c

---
+++ sum and percentage.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int phy,chem,maths,eng,cs,sum,percentage;
+printf("Enter the marks of all 5 subjects:");
+scanf("%d%d%d%d%d",&phy,&chem,&maths,&eng,&cs);
+sum=(phy+chem+maths+eng+cs)/5;
+percentage=sum/5;
+printf("The sum and percentage of the marks are:%d and %d",sum,percentage);
+return 0;
+}
